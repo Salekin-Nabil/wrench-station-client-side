b3eb8d77464cdc9b5874362e058aa60508ebde08
Salekin-Nabil <salekinnabil@gmail.com>
2022-05-28 02:43:42 +0600

    bug fixed

--- src/components/Dashboard/Payment.js
+++ src/components/Dashboard/Payment.js
@@ -7,7 +7,7 @@ import Loading from '../Loading/Loading';
 const Payment = () => {
     const {id} = useParams();
 
-    const url = `http://localhost:5000/order/${id}`;
+    const url = `http://localhost:5000/orders/${id}`;
 
     const { data: order, isLoading } = useQuery(['order', id], () => fetch(url, {
         method: 'GET',
@@ -25,7 +25,30 @@ const Payment = () => {
                 <title>Wrench Station-Payment</title>
             </Helmet>
             <h1 className='text-[#20242c] text-5xl mb-8 font-bold shadow-lg shadow-[gray] hover:shadow-xl hover:shadow-[gray] mx-[1vw] py-[1vw] rounded-lg'>Payment <span className='text-[goldenrod]'>Section</span></h1>
-            <h3 className='text-3xl ml-4'>Please pay for: {id}</h3>
+            <div class="flex justify-center mb-8">
+                <div class="block p-6 rounded-lg shadow-lg bg-white max-w-sm text-left">
+                    <h5 class="text-[goldenrod] font-bold text-2xl leading-tight mb-2">Hello, {order.buyerName}!</h5>
+                    <p class="text-[#20242c] text-xl mb-4">
+                        You have ordered <span className='text-[goldenrod] font-semibold'>{order.quantity} {order.productName}</span>
+                    </p>
+                    <p class="text-[#20242c] text-xl mb-4">
+                        Please pay <span className='text-[goldenrod] font-semibold'>${order.bill}.00</span>
+                    </p>
+                    {/* <button type="button" class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Button</button> */}
+                </div>
+            </div>
+            <div class="flex justify-center mb-24">
+                <div class="block p-6 rounded-lg shadow-lg bg-white max-w-sm text-left">
+                    <h5 class="text-[goldenrod] font-bold text-2xl leading-tight mb-2">Hello, {order.buyerName}!</h5>
+                    <p class="text-[#20242c] text-xl mb-4">
+                        You have ordered <span className='text-[goldenrod] font-semibold'>{order.quantity} {order.productName}</span>
+                    </p>
+                    <p class="text-[#20242c] text-xl mb-4">
+                        Please pay <span className='text-[goldenrod] font-semibold'>${order.bill}.00</span>
+                    </p>
+                    {/* <button type="button" class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Button</button> */}
+                </div>
+            </div>
         </div>
     );
 };
